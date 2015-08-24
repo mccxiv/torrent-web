71eae58d3c8156224d227c59bfd4f06ff8a18b38
Andrea Stella <adr.stella@gmail.com>
2015-08-24 14:49:23 -0600

    Commit first working prototype

--- main.js
+++ main.js
@@ -1,19 +1,74 @@
 var WebTorrent = require('webtorrent');
+var torrentStream = require('torrent-stream');
+var express = require('express');
+var request = require('request').defaults({encoding: null});
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var mem = require('torrent-memory-storage');
+var client;
 
-var client = new WebTorrent({
-	//storage: false
+server.listen(80);
+app.use(express.static('public'));
+
+//===============================
+// API
+//===============================
+
+io.on('connection', function (socket) {
+	console.log('New socket connection.');
+	socket.on('add-torrent', addTorrent);
+	socket.on('remove-torrent', removeTorrent);
 });
-//var magnetUri = 'http://bt.ocremix.org/torrents/sonic2project_-_Hedgehog_Heaven.torrent';
-var magnetUri = 'http://bt.ocremix.org/torrents/dkcproject-Kong_in_Concert_CD_Album.torrent';
 
-client.add(magnetUri, function (torrent) {
-	// Got torrent metadata!
-	console.log('Client is downloading:', torrent.infoHash);
+app.get('/torrent/:index', function(req, res) {
+	console.log('Torrent file request.');
+	var file = client.files[req.params.index];
+	var stream = file.createReadStream();
+	stream.pipe(res);
+});
 
-	torrent.files.forEach(function(file) {
-		file.deselect();
+//===============================
+// Main functions
+//===============================
+
+function addTorrent(url) {
+	removeTorrent();
+	request.get(url, function(err, res, body) {
+		client = torrentStream(body, {storage: mem});
+		client.ready(torrentReady);
 	});
+}
 
-	var server = torrent.createServer();
-	server.listen(80); // start the server listening to a port
-});
+function removeTorrent() {
+	if (client) {
+		client.destroy();
+		io.emit('torrent-removed');
+	}
+}
+
+//===============================
+// Helper functions
+//===============================
+
+function torrentReady() {
+	console.log('Client loaded:', client.infoHash);
+	io.emit('torrent', simplifyFilesArray(client.files));
+}
+
+function simplifyFilesArray(files) {
+	return files.map(function(file) {
+		return {
+			name: file.name,
+			path: file.path,
+			length: file.length
+		};
+	});
+}
+
+
+
+
+//var magnetUri = 'http://bt.ocremix.org/torrents/sonic2project_-_Hedgehog_Heaven.torrent';
+//var magnetUri = 'http://bt.ocremix.org/torrents/dkcproject-Kong_in_Concert_CD_Album.torrent';
+
